5516fbd4a1ffab7e94181568869cee065aa99d86
Navpreet Singh <navpreetg477@gmail.com>
2026-04-23 11:33:39 -0500

    adding auth and solving the errors for the code.

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,11 +1,12 @@
 import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
-    windowMs: 60 * 1000,
-    max: 2,
+    windowMs: 15 * 60 * 1000,
+    max: 5,
     message: {
         message: "Too many requests, please try again later"
-    }
+    },
+    skip: () => process.env.NODE_ENV === "test"
 });
 
 export default rateLimiter;
